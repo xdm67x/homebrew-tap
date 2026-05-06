cbba95a5dd97d7bba555eb1822ce8437d6e6d5f6
xdm67x <m-ozkan@outlook.fr>
2026-05-06 22:19:44 +0200

    electrotest: update to 0.4.0

--- Formula/electrotest.rb
+++ Formula/electrotest.rb
@@ -1,19 +1,19 @@
 class Electrotest < Formula
   desc "CLI tool for testing Electron applications using Gherkin syntax"
   homepage "https://github.com/xdm67x/electrotest"
-  version "0.2.0"
+  version "0.4.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/xdm67x/electrotest/releases/download/v0.2.0/electrotest-macos-arm64.tar.gz"
-      sha256 "ca8d738f6c91078e9a5961c51b949dac2b39948fb892e2f55dab82edf3757f3d"
+      url "https://github.com/xdm67x/electrotest/releases/download/v0.4.0/electrotest-macos-arm64.tar.gz"
+      sha256 "3b3f9e7d42b838ff80ecaacf76fb8183741207f5dc8120aa3df6fde90e474868"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/xdm67x/electrotest/releases/download/v0.2.0/electrotest-linux-x64.tar.gz"
-      sha256 "10ddca30e064d601f827b235d2a9c6de8dd75d1a59c6490b865f2b4cdcf2cbd1"
+      url "https://github.com/xdm67x/electrotest/releases/download/v0.4.0/electrotest-linux-x64.tar.gz"
+      sha256 "787ce7e117a530f19bc1d7d6b89198a4a52573c98330fb03bce79a596778d198"
     end
   end
 
